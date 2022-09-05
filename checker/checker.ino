31c13cb84b7ec8bfff1ee0f5ca661c2547b1509f
Ben Arc <ben@lnbits.com>
2022-09-06 00:43:53 +0100

    Changed byte to int

--- checker/checker.ino
+++ checker/checker.ino
@@ -10,7 +10,7 @@
 #define BTN1 39
 #define BTN2 38
 #define RX 2
-#define TX 4
+#define TX 5
 #define SerialNo 1
 #define SerialMessageStr "If I had 6hrs to chop down a tree I'd spend the first 4 sharpening the axe"
 
@@ -37,7 +37,7 @@ int baudMenuNo = 0;
 
 unsigned long myTime;
 
-byte protocolArray[6] = {SERIAL_7E1, SERIAL_7O1, SERIAL_8N1, SERIAL_8N2, SERIAL_8E1, SERIAL_8O1};
+int protocolArray[6] = {SERIAL_7E1, SERIAL_7O1, SERIAL_8N1, SERIAL_8N2, SERIAL_8E1, SERIAL_8O1};
 String protocolArrayStr[6] = {"SERIAL_7E1", "SERIAL_7O1", "SERIAL_8N1", "SERIAL_8N2", "SERIAL_8E1", "SERIAL_8O1"};
 int baudArray[16] = {600, 1200, 1800, 2400, 4800, 9600, 19200 ,28800 ,38400 , 57600, 76800, 115200, 230400, 460800, 576000, 921600};
 
@@ -52,7 +52,7 @@ void setup()
   
   tft.init();
   tft.setRotation(1);
-  tft.invertDisplay(false);
+  tft.invertDisplay(true);
   
   printMessage(protocolArrayStr[protocolMenuNo], "", TFT_GREEN, TFT_BLACK);
   
@@ -121,15 +121,10 @@ void setup()
 void loop()
 {
   while (SerialPort.available()) {
-    printMessage("Sending message", "", TFT_WHITE, TFT_BLACK);
-    delay(5000);
+    String x = SerialPort.readString();
+    printMessage(x, "", TFT_WHITE, TFT_BLACK);
     SerialPort.println(SerialMessageStr);
-    printMessage(String(SerialPort.read()), "", TFT_WHITE, TFT_BLACK);
-    delay(5000);
   }
-  
-  printMessage("Connection failed", "", TFT_WHITE, TFT_BLACK);
-  delay(5000);
 }
 
 void printMessage(String text1, String text2, int ftcolor, int bgcolor)
